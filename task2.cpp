e0dc026e1413960ac6dc2721c11fa0da842c4b0d
stifler33 <bagi33.ru@gmail.com>
2021-04-21 23:11:28 +0300

    task 2 chekInput

--- task2.cpp
+++ task2.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
+char userAns = '0';
 char player1;
 char player2;
+int x=0;
+int y=0;
 char playingField[3][3] = {'0','0','0',
                            '0','0','0',
                            '0','0','0',};
@@ -15,11 +18,25 @@ void coutField(){
         cout << endl;
     }
 }
-char chekWin(){
-
+bool chekWin(){
+    return false;
+}
+bool chekInput(int player){
+    if (playingField[y][x] == '0'){
+        if (player == 1 && userAns == player1 || player == 2 && userAns == player2){
+            playingField[y][x] = userAns;
+            coutField();
+            return true;
+        }else {
+            cout << "incorrect input !\n";
+            return false;
+        }
+    }else {
+        cout << "incorrect input !\n";
+        return false;
+    }
 }
 int main() {
-    char userAns = '0';
     bool corUsAns = (userAns == 'O' || userAns == 'X');
     while (!corUsAns){
         cout << "who is 1 player ? :";
@@ -37,6 +54,14 @@ int main() {
         player1 = userAns;
     }
     player1 == 'X' ? player2 = 'O' : player2 = 'X';
+    cout << "input format - x y X or O\n";
+
+        cout << "go 1 player\n";
+        cin >> x >> y >> userAns;
+        chekInput(1);
+        cout << "go 2 player\n";
+        cin >> x >> y >> userAns;
+        chekInput(2);
 
     return 0;
 }
