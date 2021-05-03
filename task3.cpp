96df326a1467cc303e851f6a334c037c45b5f43c
stifler33 <bagi33.ru@gmail.com>
2021-05-03 18:36:20 +0300

    task 3 final

--- task3.cpp
+++ task3.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int array[5][5];
+    int val = 24;
+    for (int l = 4; l >= 0; l--){
+        for (int c = 4; c >= 0 && (l%2 == 0); c--){
+            array[l][c] = val--;
+        }
+        for (int c = 0; c < 5 && (l%2 != 0); c++){
+            array[l][c] = val--;
+        }
+    }
+    for (int i = 0; i < 5; i++){
+        for (int g = 0; g < 5; g++){
+            std::cout << array[i][g] << " ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
