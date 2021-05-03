319f1b2a4208c778763fabd1afbabef9dc9a2276
stifler33 <bagi33.ru@gmail.com>
2021-05-03 19:55:18 +0300

    task 5 init

--- task5.cpp
+++ task5.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int array[4][4] = {{0,0,0,0},
+                       {0,0,0,0},
+                       {0,0,0,0},
+                       {0,0,0,0}};
+    std::cout << "Enter diagonal\n";
+    int val;
+    for (int l = 0; l < 4; l++){
+        std::cin >> val;
+        array[l][l] = val;
+    }
+    for (int l = 0; l < 4; l++){
+        for (int c = 0; c < 4; c++){
+            std::cout << array[l][c] << " ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
