07cd4c9e55738d10d8d8975f8ac14479a9071fbf
stifler33 <bagi33.ru@gmail.com>
2021-05-10 17:38:14 +0300

    task 8 init

--- task7.cpp
+++ task7.cpp
@@ -8,7 +8,7 @@ void initField(bool fieldPop[][12]){
     }
 }
 void outputField(bool fieldPop[][12]){
-    for (int l = 0; l < 12; l++){
+    for (int l = 11; l >= 0; l--){
         for (int c = 0; c < 12; c++){
             fieldPop[l][c] == true ? cout << 'O' : cout << 'X';
             cout << " ";
@@ -18,7 +18,7 @@ void outputField(bool fieldPop[][12]){
 }
 bool burstField(bool fieldPop[][12], int &x,int &y,int &eX,int &eY){
     if ((x > 12 || x < 0) || (y > 12 || y < 0) || (eX > 12 || eX < 0) || (eY > 12 || eY < 0)){
-        cout << "Error !";
+        cout << "Error !\n";
         return false;
     }else {
         x -= 1;
@@ -28,31 +28,33 @@ bool burstField(bool fieldPop[][12], int &x,int &y,int &eX,int &eY){
         for (int l = 0; l < 12; l++) {
             for (int c = 0; c < 12; c++) {
                 if ((l >= y && l <= eY) && (c >= x && c <= eX)) {
-                    if (fieldPop[l][c] == true){
                         fieldPop[l][c] = false;
-                    }else return false;
                 }
             }
         }
     }
     return true;
 }
-
+bool exit(bool fieldPop[][12]){
+    for (int l = 0; l < 12; l++) {
+        for (int c = 0; c < 12; c++) {
+            if (fieldPop[l][c] == true) return true;
+        }
+    }
+    return false;
+}
 int main() {
     bool fieldPop[12][12];
     initField(fieldPop);
     outputField(fieldPop);
-    int x;
-    int y;
-    int eX;
-    int eY;
-    do{
-
+    while (exit(fieldPop)){
+        int x;
+        int y;
+        int eX;
+        int eY;
         cout << "Enter X and Y start:", cin >> x >>y;
-
         cout << "Enter X and Y end:", cin >> eX >>eY;
-        outputField(fieldPop);
-    }while (burstField(fieldPop, x,y,eX,eY));
-    //if(burstField(fieldPop, x,y,eX,eY)) outputField(fieldPop);
+        if(burstField(fieldPop, x,y,eX,eY)) outputField(fieldPop);
+    }
     return 0;
 }
--- task8.cpp
+++ task8.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+void initField(bool field[][5][5]){
+    for (int z = 0; z < 10; z++){
+        for (int y = 0; y < 5; y++){
+            for (int x = 0; x < 5; x++){
+                field[z][y][x] = false;
+            }
+        }
+    }
+}
+void enterHeightBlock(bool field[][5][5], int &heightBlock){
+    for (int z = 0; z < 10; z++){
+        for (int y = 0; y < 5; y++){
+            for (int x = 0 ; x < 5; x++){
+                if (z < heightBlock){
+                    field[z][y][x] = true;
+                }else field[z][y][x] = false;
+            }
+        }
+    }
+}
+void coutField(bool field[][5][5]){
+    for (int z = 0; z < 10; z++){
+        for (int y = 0; y < 5; y++){
+            for (int x = 0 ; x < 5; x++){
+                cout << field[z][y][x] << " ";
+            }
+            cout << endl;
+        }
+        cout << z << " *********************\n";
+    }
 
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    bool field[10][5][5];
+    initField(field);
+    int heightBlock = 0;
+
+    //coutField(field);
+
     return 0;
 }
