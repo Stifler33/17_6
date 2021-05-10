4f987c941db1ed1911c25b8e5b840cd1dc99ce5f
stifler33 <bagi33.ru@gmail.com>
2021-05-10 20:24:16 +0300

    task 8 cout slice

--- task8.cpp
+++ task8.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 using namespace std;
 void initField(bool field[][5][5]){
     for (int z = 0; z < 10; z++){
@@ -25,11 +26,25 @@ void coutField(bool field[][5][5]){
     }
 
 }
+void coutSlice(bool field[][5][5], int numberSlice){
+    assert(numberSlice <= 9 && numberSlice >= 0);
+    for (int y = 0; y < 5; y++){
+        for (int x = 0; x < 5; x++){
+            cout << field[numberSlice][y][x] << " ";
+        }
+        cout << endl;
+    }
+}
 int main() {
     bool field[10][5][5];
     initField(field);
-    int hBlock[25] = {9,9,8,7,5,4,3,2,1,0,1,8,7,9,5,4,6,8,9,6,4,1,0,3,10};
-    int heightBlock = 0;
+    //vector<int> hBlock = {9,9,8,7,5,4,3,2,1,0,1,8,7,10,5,4,6,8,9,6,4,1,0,3,10};
+    vector<int> hBlock(25);
+    for (int i = 0; i < hBlock.size(); i++){
+        cout << "enter height block " << i << " : ";
+        cin >> hBlock[i];
+        assert(hBlock[i] <= 10 && hBlock[i] >= 0);
+    }
     int countBlock = 0;
     for (int y = 0; y < 5; y++){
         for (int x = 0; x < 5; x++){
@@ -39,7 +54,13 @@ int main() {
             countBlock++;
         }
     }
-    coutField(field);
+    //coutField(field);
+    cout << "Enter number slice : ";
+    int slice = 0;
+    while (slice != -1){
+        cin >> slice;
+        if (slice >= 0 && slice < 10) coutSlice(field, slice);
+    }
 
     return 0;
 }
