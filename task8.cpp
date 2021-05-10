14bd7aa9ae0b7fb85892ac4bcaf6b07ae233fc9e
stifler33 <bagi33.ru@gmail.com>
2021-05-10 19:30:48 +0300

    task 8 init block

--- task8.cpp
+++ task8.cpp
@@ -10,35 +10,36 @@ void initField(bool field[][5][5]){
         }
     }
 }
-void enterHeightBlock(bool field[][5][5], int &heightBlock){
-    for (int z = 0; z < 10; z++){
-        for (int y = 0; y < 5; y++){
-            for (int x = 0 ; x < 5; x++){
-                if (z < heightBlock){
-                    field[z][y][x] = true;
-                }else field[z][y][x] = false;
-            }
-        }
-    }
+void enterHeightBlock(bool field[][5][5], int &heightBlock, int x, int y){
+
 }
 void coutField(bool field[][5][5]){
     for (int z = 0; z < 10; z++){
+        cout << z << " *********************\n";
         for (int y = 0; y < 5; y++){
             for (int x = 0 ; x < 5; x++){
                 cout << field[z][y][x] << " ";
             }
             cout << endl;
         }
-        cout << z << " *********************\n";
     }
 
 }
 int main() {
     bool field[10][5][5];
     initField(field);
+    int hBlock[25] = {9,9,8,7,5,4,3,2,1,0,1,8,7,9,5,4,6,8,9,6,4,1,0,3,10};
     int heightBlock = 0;
-
-    //coutField(field);
+    int countBlock = 0;
+    for (int y = 0; y < 5; y++){
+        for (int x = 0; x < 5; x++){
+            for (int z = 0; z < hBlock[countBlock]; z++){
+                field[z][y][x] = true;
+            }
+            countBlock++;
+        }
+    }
+    coutField(field);
 
     return 0;
 }
