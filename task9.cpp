6585aa3dae84219610a86ad5784862706b0219bd
stifler33 <bagi33.ru@gmail.com>
2021-05-11 23:36:56 +0300

    task 9 checkout

--- task9.cpp
+++ task9.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void initField(bool field_1[][10], bool field_2[][10]){
+void init_Field_Player(vector<vector<int>> &field, int numberPlayer){
+    int originX;
+    int originY;
+    int endX;
+    int endY;
+    do{
+        cout << "enter origin 4-deck ship : \n";
+        cin >> originX >> originY;
+        cout << "enter end 4-deck ship : \n";
+        cin >> endX >> endY;
+        bool out = originX != endX && originY != endY;
+        if (out){
+            cout << "only vertical or horizontal !\n";
+        }
+    }while (out);
+
 
 }
 int main() {
-    vector<vector<bool>> play1(10,vector<bool> (10, false));
-    bool field_1 [10][10];
-    bool field_2 [10][10];
-    vector<bool> vec(10);
+    vector<vector<int>> field1(10,vector<int> (10, 0));
+    vector<vector<int>> field2(10,vector<int> (10, 0));
+    init_Field_Player(field1,1);
+
     /*
      for (int l = 0; l < play1.size(); l++){
         for (int c : play1[l]){
