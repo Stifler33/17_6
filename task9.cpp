a3cba7fb2ada158a71261b1e0239ba237482b6bd
stifler33 <bagi33.ru@gmail.com>
2021-05-11 22:16:03 +0300

    task 9 init

--- task9.cpp
+++ task9.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+void initField(bool field_1[][10], bool field_2[][10]){
 
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<vector<bool>> play1(10,vector<bool> (10, false));
+    bool field_1 [10][10];
+    bool field_2 [10][10];
+    vector<bool> vec(10);
+    /*
+     for (int l = 0; l < play1.size(); l++){
+        for (int c : play1[l]){
+            cout << play1[l][c] << " ";
+        }
+        cout << endl;
+    }
+     */
+
     return 0;
 }
