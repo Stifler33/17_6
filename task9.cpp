79c65e1a8cc3f8d1d44c279583ca59dc8122ccb9
stifler33 <bagi33.ru@gmail.com>
2021-05-12 22:06:22 +0300

    task 9 added in field 4-deck ship

--- task9.cpp
+++ task9.cpp
@@ -2,28 +2,45 @@
 #include <vector>
 using namespace std;
 void init_Field_Player(vector<vector<int>> &field, int numberPlayer){
-    int originX;
-    int originY;
-    int endX;
-    int endY;
+    int originX = 0;
+    int originY = 0;
+    int endX = 0;
+    int endY = 0;
+    bool out;
     do{
         cout << "enter origin 4-deck ship : \n";
         cin >> originX >> originY;
         cout << "enter end 4-deck ship : \n";
         cin >> endX >> endY;
-        bool out = originX != endX && originY != endY;
+        out = originX != endX && originY != endY;
         if (out){
             cout << "only vertical or horizontal !\n";
         }
     }while (out);
+    if (originX == endX){
+        for (int l = originY; l < endY; l++){
+            field[l][originX] = 1;
+        }
+    }else if (originY == endY){
+        for (int c = originX; c < endX; c++){
+            field[originY][c] = 1;
+        }
+    }
 
-
+}
+void cout_Field(vector<vector<int>> &field){
+    for (int l = 0; l < field.size(); l++){
+        for (int c = 0; c <  field[l].size(); c++){
+            cout << field[l][c] << " ";
+        }
+        cout << endl;
+    }
 }
 int main() {
     vector<vector<int>> field1(10,vector<int> (10, 0));
     vector<vector<int>> field2(10,vector<int> (10, 0));
     init_Field_Player(field1,1);
-
+    cout_Field(field1);
     /*
      for (int l = 0; l < play1.size(); l++){
         for (int c : play1[l]){
