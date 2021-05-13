7eb7760d56b86af89897fad22153169ac68387b6
stifler33 <bagi33.ru@gmail.com>
2021-05-13 22:34:52 +0300

    task 9 init ship

--- task9.cpp
+++ task9.cpp
@@ -1,45 +1,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void init_Field_Player(vector<vector<int>> &field, int numberPlayer){
-    int originX = 0;
-    int originY = 0;
-    int endX = 0;
-    int endY = 0;
-    bool out;
-    do{
-        cout << "enter origin 4-deck ship : \n";
-        cin >> originX >> originY;
-        cout << "enter end 4-deck ship : \n";
-        cin >> endX >> endY;
-        out = originX != endX && originY != endY;
-        if (out){
-            cout << "only vertical or horizontal !\n";
-        }else {
-             if (originX == endX){
-                for (int l = originY; l <= endY; l++){
-                    if (field[l][originX] == 0) {
-                        field[l][originX] = 1;
+bool init_Field_Player(vector<vector<int>> &field, int &inX, int &inY, int &endX, int &endY){
+             if (inX == endX){
+                for (int l = inY; l <= endY; l++){
+                    if (field[l][inX] == 0) {
+                        field[l][inX] = 1;
                     }else {
                         cout << "already here ship!\n";
-                        out = true;
+                        return false;
                     }
                 }
-            }else if (originY == endY){
-                for (int c = originX; c <= endX; c++){
-                    if (field[originY][c] == 0) {
-                        field[originY][c] = 1;
+            }else if (inY == endY){
+                for (int c = inX; c <= endX; c++){
+                    if (field[inY][c] == 0) {
+                        field[inY][c] = 1;
                     }else {
                         cout << "already here ship!\n";
-                        out = true;
+                        return false;
                     }
                 }
             }
+    return true;
         }
-    }while (out);
 
-
-}
 void cout_Field(vector<vector<int>> &field){
     for (int l = 0; l < field.size(); l++){
         for (int c = 0; c <  field[l].size(); c++){
@@ -51,16 +35,32 @@ void cout_Field(vector<vector<int>> &field){
 int main() {
     vector<vector<int>> field1(10,vector<int> (10, 0));
     vector<vector<int>> field2(10,vector<int> (10, 0));
-    init_Field_Player(field1,1);
-    cout_Field(field1);
-    /*
-     for (int l = 0; l < play1.size(); l++){
-        for (int c : play1[l]){
-            cout << play1[l][c] << " ";
-        }
-        cout << endl;
+    int originX = 0;
+    int originY = 0;
+    int endX = 0;
+    int endY = 0;
+    bool corCoordinates;
+    int dockShip = 4;
+    while (dockShip > 0){
+        do{
+            if (dockShip == 1){
+                cout << "enter 1 dock ship ";
+                cin >> originX >> originY;
+                endX = originX;
+                endY = originY;
+            }else {
+                cout << "enter origin x and y " << dockShip << " dock ship\n";
+                cin >> originX >> originY;
+                cout << "enter end x and y " << dockShip << " dock ship\n";
+                cin >> endX >> endY;
+                corCoordinates = originX != endX && originY != endY;
+            }
+        }while(!init_Field_Player(field1,originX,originY,endX,endY) || corCoordinates);
+        dockShip --;
     }
-     */
+
+    cout_Field(field1);
+
 
     return 0;
 }
