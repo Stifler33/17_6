f4b67c8f723c78a6ce4c6a44657d3c2e558b029d
stifler33 <bagi33.ru@gmail.com>
2021-05-16 18:57:45 +0300

    task 9 finish

--- task9.cpp
+++ task9.cpp
@@ -6,32 +6,32 @@ int originX = 0;
 int originY = 0;
 int endX = 0;
 int endY = 0;
-bool init_Field_Player(vector<vector<int>> &field, int &inX, int &inY, int &endX, int &endY){
+bool init_Field_Player(vector<vector<char>> &field, int &inX, int &inY, int &endX, int &endY){
     assert(inX < 10 && inY < 10 && endX < 10 && endY < 10);
             for (int iterationCount = 0; iterationCount < 2; iterationCount++){
                 if (inX == endX){
                     for (int l = inY; l <= endY; l++){
-                        if (field[l][inX] != 0) {
+                        if (field[l][inX] != ' ') {
                             cout << "already here ship!\n";
                             return false;
                         }else if (iterationCount == 1){
-                            field[l][inX] = 1;
+                            field[l][inX] = '*';
                         }
                     }
                 }else if (inY == endY){
                     for (int c = inX; c <= endX; c++){
-                        if (field[inY][c] != 0) {
+                        if (field[inY][c] != ' ') {
                             cout << "already here ship!\n";
                             return false;
                         }else if (iterationCount == 1){
-                            field[inY][c] = 1;
+                            field[inY][c] = '*';
                         }
                     }
                 }
             }
     return true;
         }
-void input_ship(int &dockShip, vector<vector<int>> &field){
+void input_ship(int &dockShip, vector<vector<char>> &field){
     bool corCoordinates = true;
     do{
         if (dockShip == 1){
@@ -56,7 +56,7 @@ void input_ship(int &dockShip, vector<vector<int>> &field){
         if (!corCoordinates) cout << "incorrect coordinates\n";
     }while(!corCoordinates || !init_Field_Player(field,originX,originY,endX,endY));
 }
-void count_ship(vector<vector<int>> &field){
+void count_ship(vector<vector<char>> &field){
     int dockShip = 1;
     while (dockShip <= 4){
 
@@ -78,7 +78,7 @@ void count_ship(vector<vector<int>> &field){
         dockShip++;
     }
 }
-void cout_Field(vector<vector<int>> &field){
+void cout_Field(vector<vector<char>> &field){
     for (int l = 0; l < field.size(); l++){
         for (int c = 0; c <  field[l].size(); c++){
             cout << field[l][c] << " ";
@@ -87,17 +87,61 @@ void cout_Field(vector<vector<int>> &field){
     }
     cout << "\n*************************\n";
 }
+int shots_Left(vector<vector<char>> &field){
+    int countShot = 0;
 
+    for (int l = 0; l < field.size(); l++){
+        for (int c = 0; c <  field[l].size(); c++){
+            if (field[l][c] == '*') countShot++;
+        }
+    }
+    return countShot;
+}
+bool shot_player(vector<vector<char>> &field, int player){
+    int x, y;
+    cout << "shoot player " << player << endl;
+    cin >> x >> y;
+    assert((x >= 0 && x < 10) && (y >= 0 && y < 10));
+    if (field[y][x] == '*') {
+        cout << "hit! Shot again\n";
+        field[y][x] = ' ';
+        return true;
+    } else if (field[y][x] == ' ') {
+        cout << "missed :(\n";
+        return false;
+    }
+}
 int main() {
-    vector<vector<int>> field1(10,vector<int> (10, 0));
-    vector<vector<int>> field2(10,vector<int> (10, 0));
+    vector<vector<char>> field1(10,vector<char> (10, ' '));
+    vector<vector<char>> field2(10,vector<char> (10, ' '));
     count_ship(field1);
-    //count_ship(field2);
-
-
+    count_ship(field2);
+    int winPlayer = 0;
+    int x, y;
+    bool win = false;
+    while(!win){
+            while(!win && shot_player(field1, 1)){
+                if (shots_Left(field1) == 0 || shots_Left(field2) == 0){
+                    win = true;
+                    break;
+                }
+            }
+            while(!win && shot_player(field2, 2)){
+                if (shots_Left(field1) == 0 || shots_Left(field2) == 0){
+                    win = true;
+                    break;
+                }
+            }
+    }
+    if (shots_Left(field1) == 0){
+        winPlayer = 1;
+    }else if (shots_Left(field2) == 0){
+        winPlayer = 2;
+    }
+    cout << "Win player " << winPlayer;
 
     cout_Field(field1);
-    //cout_Field(field2);
+    cout_Field(field2);
 
     return 0;
 }
